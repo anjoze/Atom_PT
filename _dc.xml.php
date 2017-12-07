07cccc75cfae3ba4cecd14a5059f63090ced76c0
José Anjos <joseanjos@gmail.com>
2017-12-07 13:57:09 +0000

    alterado para rede PT

--- _dc.xml.php
+++ _dc.xml.php
@@ -17,29 +17,44 @@
 
   <dc:description><?php echo esc_specialchars(strval($resource->scopeAndContent)) ?></dc:description>
 
-  <?php foreach ($resource->getPublishers() as $item): ?>
-    <dc:publisher><?php echo esc_specialchars(strval($item)) ?></dc:publisher>
-  <?php endforeach; ?>
+  <?php if (isset($resource->repository)): ?>
+    <dc:publisher><?php echo url_for(array($resource->repository, ‘module’ => ‘repository’), true) ?></dc:publisher>
+    <dc:publisher><?php echo esc_specialchars(strval($resource->repository)) ?></dc:publisher>
+  <?php endif; ?>
 
   <?php foreach ($resource->getContributors() as $item): ?>
     <dc:contributor><?php echo esc_specialchars(strval($item)) ?></dc:contributor>
   <?php endforeach; ?>
 
-  <?php foreach ($dc->date as $item): ?>
-    <dc:date><?php echo esc_specialchars(strval($item)) ?></dc:date>
+  <?php foreach ($resource->getDates() as $item): ?>
+    <dc:date><?php echo Qubit::renderDate($item->startDate) ?></dc:date>
   <?php endforeach; ?>
 
-  <?php foreach ($dc->type as $item): ?>
-    <dc:type><?php echo esc_specialchars(strval($item)) ?></dc:type>
+  <?php foreach ($resource->getDates() as $item): ?>
+    <dc:date><?php echo Qubit::renderDate($item->endDate) ?></dc:date>
   <?php endforeach; ?>
+
+  <?php
+    foreach ($dc->date as $item1) {
+     foreach ($resource->getDates() as $item2) {
+     }
+    }
+    if (esc_specialchars(strval($item1)) != Qubit::renderDate($item2->startDate)."-".Qubit::renderDate($item2->endDate)) {
+      echo "<dc:date>";
+      echo esc_specialchars(strval($item1));
+      echo "</dc:date>";
+    }
+  ?>
+
+  <dc:type><?php echo esc_specialchars($levelOfDescription = $resource->getLevelOfDescription()->getName(array(‘cultureFallback’ => true))) ?></dc:type>
 
   <?php foreach ($dc->format as $item): ?>
     <dc:format><?php echo esc_specialchars(strval($item)) ?></dc:format>
   <?php endforeach; ?>
 
-  <dc:identifier><?php echo esc_specialchars(sfConfig::get('app_siteBaseUrl') .'/'.$resource->slug) ?></dc:identifier>
+  <dc:identifier><?php echo esc_specialchars (strval($dc->identifier))?></dc:identifier>
 
-  <dc:identifier><?php echo esc_specialchars(strval($resource->identifier)) ?></dc:identifier>
+  <dc:identifier><?php echo esc_specialchars(url_for(array($resource, 'module' => 'informationobject'), true)) ?></dc:identifier>
 
   <dc:source><?php echo esc_specialchars(strval($resource->locationOfOriginals)) ?></dc:source>
 
@@ -47,10 +62,13 @@
     <dc:language xsi:type="dcterms:ISO639-3"><?php echo esc_specialchars(strval(strtolower($iso639convertor->getID3($code)))) ?></dc:language>
   <?php endforeach; ?>
 
-  <?php if (isset($resource->repository)): ?>
-    <dc:relation><?php echo esc_specialchars(url_for(array($resource->repository, 'module' => 'repository'), true)) ?></dc:relation>
-    <dc:relation><?php echo esc_specialchars(strval($resource->repository->authorizedFormOfName)) ?></dc:relation>
-  <?php endif; ?>
+  <?php
+     if (isset($resource->digitalObjects[0])) {
+     echo "<dc:relation>http://";
+     echo $withoutExt = preg_replace('/\\.[^.\\s]{3,4}$/', '',$sf_request->getHost().$sf_request->getRelativeUrlRoot().$resource->digitalObjects[0]->getFullPath());
+     echo "_142.jpg</dc:relation>";
+     }
+  ?>
 
   <?php foreach ($dc->coverage as $item): ?>
     <dc:coverage><?php echo esc_specialchars(strval($item)) ?></dc:coverage>
